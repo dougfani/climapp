ccf430849e15aec48efa9d5f087e76d7b46df136
Douglas Floriano <douglas97floriano@gmail.com>
2026-06-26 16:37:13 -0300

    Add connection to the API, makes te initial request to fetch weather data, and displays it

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,42 @@
-import SearchBar from "./components/SearchBar";
-import WeatherCard from "./components/WeatherCard";
-import "./App.css";
-import { useState } from "react";
-import { useEffect } from "react";
+import SearchBar from './components/SearchBar';
+import WeatherCard from './components/WeatherCard';
+import './App.css';
+import { useEffect, useState } from 'react';
 
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 function App() {
-  const [weather, setWeather] = useState(null);
+    const [weather, setWeather] = useState(null);
 
-  useEffect(() => {
-    async function fetchWeather() {
-      try {
-        const response = await fetch(
-          `https://api.hgbrasil.com/weather?format=json-cors&key=${API_KEY}&city_name=São Luís, MA`
-        );
-        const data = await response.json();
-        if (data.results) {
-          setWeather(data.results);
+    useEffect(() => {
+        async function fetchWeather() {
+            try {
+                const response = await fetch(
+                    `https://api.hgbrasil.com/weather?format=json-cors&key=${API_KEY}&city_name=Palmas, TO`,
+                );
+                const data = await response.json();
+                if (data.results) {
+                    setWeather(data.results);
+                }
+            } catch (error) {
+                console.error('Erro ao buscar dados da API', error);
+            }
         }
-      } catch (erro) {
-        console.error("Erro ao buscar dados da API", erro);
-      }
-    }
 
-    fetchWeather();
-  }, []);
+        fetchWeather();
+    }, []);
 
-  return (
-    <div className="app-container">
-      <SearchBar />
-      {weather && (
-        <>
-          <h1>{weather.city}</h1>
-          <WeatherCard weather={weather} />
-        </>
-      )}
-    </div>
-  );
+    return (
+        <div className="app-container">
+            <SearchBar />
+            {weather && (
+                <>
+                    <h1>{weather.city}</h1>
+                    <WeatherCard weather={weather} />
+                </>
+            )}
+        </div>
+    );
 }
 
 export default App;
