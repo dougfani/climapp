00c23248e87f43408e6b29e76f165239840f51bd
Douglas Floriano <douglas97floriano@gmail.com>
2026-06-26 19:05:59 -0300

    add ForecastList and ForecastItem components to show forecast. Update fetc logic to include forecast

---
+++ src/components/ForecastItem/index.jsx
@@ -0,0 +1,16 @@
+import './styles.css';
+
+const ForecastItem = ({ date, min, max, condition, description }) => {
+    return (
+        <div className='forecast-item'>
+            <p className='forecast-day'>({date})
+            </p>
+            <img src={`./icons-weather/${condition}.svg`} alt={description} />
+            <p className='forecast-temp'>
+                {min}/{max}°
+            </p>
+        </div>
+    );
+}
+
+export default ForecastItem
--- src/components/WeatherCard/index.jsx
+++ src/components/WeatherCard/index.jsx
@@ -2,29 +2,33 @@ import './styles.css';
 
 const WeatherCard = ({ weather }) => {
     return (
-        <section className="weather-card">
-            <p>Hoje ({weather.forecast[0].date})</p>
-            <img 
-              src={`./icons-weather/${weather.condition_slug}.svg`}
-              alt={weather.description} 
-            />
-            <h2 className="temperature">{weather.temp}°</h2>
-            <p className="condition">{weather.description}</p>
-            <div className="humidity">
-                <div>
-                    <img src="./humidity.svg" alt="" />
-                    <p>Umidade: </p>
+        <>
+            <section className="weather-card">
+                <p>Hoje ({weather.forecast[0].date})</p>
+                <img
+                    src={`./icons-weather/${weather.condition_slug}.svg`}
+                    alt={weather.description}
+                />
+                <h2 className="temperature">{weather.temp}°</h2>
+                <p className="condition">{weather.description}</p>
+                <div className="humidity">
+                    <div>
+                        <img src="./humidity.svg" alt="" />
+                        <p>Umidade: </p>
+                    </div>
+                    <span>{weather.humidity} %</span>
                 </div>
-                <span>{weather.humidity} %</span>
-            </div>
-            <div className="min-max">
-                <div>
-                    <img src="./temp.svg" alt="" />
-                    <p>Min/Max:</p>
+                <div className="min-max">
+                    <div>
+                        <img src="./temp.svg" alt="" />
+                        <p>Min/Max:</p>
+                    </div>
+                    <span>
+                        {weather.forecast[0].min}/{weather.forecast[0].max}°
+                    </span>
                 </div>
-                <span>{weather.forecast[0].min}/{weather.forecast[0].max}°</span>
-            </div>
-        </section>
+            </section>
+        </>
     );
 };
 
