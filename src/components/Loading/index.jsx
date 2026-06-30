9485f0ef7c74eff84a6488d2615acb3444ff9391
Douglas Floriano <douglas97floriano@gmail.com>
2026-06-30 17:37:25 -0300

    add Loading component and implemnts application loading loic

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+const Loading = () => {
+    return (
+        <div className="loading-card">
+            <span className='searching-text'>Buscando clima...</span>
+        </div>
+    )
+}
+
+export default Loading;
